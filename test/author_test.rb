fd156606a02dddc1b107c23c5ee62f10970c6b12
RyanDBarnett <conner2468@gmail.com>
2020-07-22 13:10:14 -0600

    Add Author write tests

--- test/author_test.rb
+++ test/author_test.rb
@@ -19,4 +19,21 @@ class AuthorTest < Minitest::Test
   def test_books_should_be_empty_by_default
     assert_equal [], @author.books
   end
+
+  def test_write_creates_a_book
+    jane_eyre = @author.write("Jane Eyre", "October 16, 1847")
+
+    assert_instance_of Book, jane_eyre
+    assert_equal "Jane Eyre", jane_eyre.title
+  end
+
+  def test_write_adds_the_new_book_to_authors_books
+    jane_eyre = @author.write("Jane Eyre", "October 16, 1847")
+
+    assert_equal [jane_eyre], @author.books
+
+    villette = @author.write("Villette", "1853")
+
+    assert_equal [jane_eyre, villette], @author.books
+  end
 end
