0d50c8b88d39d8aaa333437ecdba56c5a5cc4d07
RyanDBarnett <conner2468@gmail.com>
2020-07-22 12:52:57 -0600

    Add publication year method to Book

--- lib/book.rb
+++ lib/book.rb
@@ -4,9 +4,14 @@ class Book
     @title = data[:title]
     @author_first_name = data[:author_first_name]
     @author_last_name = data[:author_last_name]
+    @publication_date = data[:publication_date]
   end
 
   def author
     @author_first_name + " " + @author_last_name
+  end
+
+  def publication_year
+    @publication_date.split(' ').last
   end
 end
