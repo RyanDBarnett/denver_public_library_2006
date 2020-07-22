a0772423a1fa4331a70bfce8069fffde9c75a331
RyanDBarnett <conner2468@gmail.com>
2020-07-22 12:48:32 -0600

    Add author method to book class

--- lib/book.rb
+++ lib/book.rb
@@ -2,5 +2,11 @@ class Book
   attr_reader :title
   def initialize data
     @title = data[:title]
+    @author_first_name = data[:author_first_name]
+    @author_last_name = data[:author_last_name]
+  end
+
+  def author
+    @author_first_name + " " + @author_last_name
   end
 end
