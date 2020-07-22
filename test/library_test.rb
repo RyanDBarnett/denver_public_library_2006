b89511a05d552ffa0cc03dccb06ea905ccb7a1d6
RyanDBarnett <conner2468@gmail.com>
2020-07-22 14:06:16 -0600

    Add Library checkout tests

--- test/library_test.rb
+++ test/library_test.rb
@@ -56,4 +56,29 @@ class LibraryTest < Minitest::Test
     assert_equal({:start=>"1847", :end=>"1857"}, @library.publication_time_frame_for(charlotte_bronte))
     assert_equal({:start=>"1960", :end=>"1960"}, @library.publication_time_frame_for(harper_lee))
   end
+
+  def test_cannot_checkout_book_if_book_not_in_library
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    assert_equal false, @library.checkout(jane_eyre)
+    assert_equal false, @library.checkout(mockingbird)
+  end
+
+  def test_cannot_checkout_book_if_book_not_in_library
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    @library.add_author(charlotte_bronte)
+    @library.add_author(harper_lee)
+
+    assert_equal true, @library.checkout(jane_eyre)
+    assert_equal true, @library.checkout(mockingbird)
+  end
 end
