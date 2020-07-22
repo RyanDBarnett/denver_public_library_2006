05ccab2ffacd1ba4beb62a9d0a1b0e559c0c87dd
RyanDBarnett <conner2468@gmail.com>
2020-07-22 13:24:45 -0600

    Add library setup and exist test

---
+++ test/library_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/book'
+require './lib/author'
+require './lib/library'
+
+class LibraryTest < Minitest::Test
+  def setup
+    @library = Library.new("Denver Public Library")
+  end
+
+  def test_it_exists
+    assert_instance_of Library, @library
+  end
+end
